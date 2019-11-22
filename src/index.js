7ab33d13bcab544bd1a42839aec59ce2f0ecae2e
Ilya Trenin <itrenin@gmail.com>
2019-11-22 16:01:31 +0300

    сделана ДЗ 3

--- src/index.js
+++ src/index.js
@@ -16,7 +16,27 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
-function isAllTrue(array, fn) {
+function isAllTrue(array, fn) { 
+  
+    if (Object.prototype.toString.call(array) != '[object Array]' || array.length == 0 ) {
+        throw new Error ('empty array');
+    }
+
+    if (typeof fn != 'function') {
+        throw new Error ('fn is not a function');
+    }
+
+    for ( let i = 0; i < array.length; i++) {
+
+        let callResult = fn(array[i]);
+
+        if (callResult === false) {
+            
+            return false;
+        }    
+    }
+
+    return true;
 }
 
 /*
@@ -36,6 +56,23 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
+    if (Object.prototype.toString.call(array) != '[object Array]' || array.length == 0 ) {
+        throw new Error ('empty array');
+    }
+    if (typeof fn != 'function') {
+        throw new Error ('fn is not a function');
+    }
+    for (let i = 0; i < array.length; i++) {
+
+        let callResult = fn(array[i]);
+
+        if (callResult == true) {
+
+            return true;
+        }
+    }
+
+    return false;
 }
 
 /*
@@ -50,6 +87,22 @@ function isSomeTrue(array, fn) {
    - fn не является функцией (с текстом "fn is not a function")
  */
 function returnBadArguments(fn) {
+    if (typeof fn != 'function') {
+        throw new Error ('fn is not a function');
+    }
+
+    let badArgumentsArray = [];
+
+    for (let i = 1; i < arguments.length; i++) {
+        try {
+            fn(arguments[i]);
+        } catch (e) {
+            badArgumentsArray.push(arguments[i]);        
+        }
+      
+    }
+
+    return badArgumentsArray;
 }
 
 /*
@@ -69,7 +122,45 @@ function returnBadArguments(fn) {
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator() {
+function calculator(number = 0) {
+    if (typeof number !== 'number') {
+        throw new Error ('number is not a number');
+    }
+
+    return {
+        sum: function() {
+            for (let i = 0; i < arguments.length; i++) {
+                number += arguments[i];
+            }
+
+            return number;
+        },
+        dif: function() {
+            for (let i = 0; i < arguments.length; i++) {
+                number -= arguments[i];
+            }
+
+            return number;
+        },
+        div: function() {
+            for (let i = 0; i < arguments.length; i++) {
+                if (arguments[i] === 0) {
+                    throw new Error ('division by 0');
+                }
+                number /= arguments[i];
+            }
+
+            return number;
+        },
+        mul: function() {
+            for (let i = 0; i < arguments.length; i++) {
+                number *= arguments[i];
+            }
+
+            return number;
+        }
+
+    };
 }
 
 /* При решении задач, пострайтесь использовать отладчик */
